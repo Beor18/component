fab9dffca3933519ca6241186b0d205ba5d8ef14
Fernando Lopez <fer.ragnar19@gmail.com>
2021-12-09 21:01:48 -0300

    fixs

--- components/button.jsx
+++ components/button.jsx
@@ -1,84 +1,85 @@
-import React, { useState } from 'react'
-import { ethers } from 'ethers'
-import { Button, Box, Heading } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { Button, Box, Heading } from "@chakra-ui/react";
 
-const Message = ({text, checkNetwork}) => {
+const Message = ({ text, checkNetwork }) => {
   return (
     <Box>
       <Heading>No tenes instalado Metamask</Heading>
     </Box>
-  )
-}
+  );
+};
 
 const ButtonAdd = () => {
-  const [chainId, setChainId] = useState(null)
-  const [id, setId] = useState(null)
+  const [chainId, setChainId] = useState(null);
+  const [id, setId] = useState(null);
 
-  // const netWork = async () => {
-  //   const provider = await new ethers.providers.Web3Provider(window.ethereum)
-  //   const getAddress = await provider.send('eth_requestAccounts', [])
+  const netWork = async () => {
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
+    const getAddress = await provider.send("eth_requestAccounts", []);
 
-  //   console.log("Fer address >>> ", getAddress)
-  //   const network = provider.getNetwork()
+    const network = provider.getNetwork();
 
-  //   network
-  //     .then((result) => {
-  //       console.log("Fer result >>> ", result)
-  //       setId({chainId: result.chainId, address: result})
-  //     })
-  //     .catch((err) => {
-  //       console.log('Message Error >>> ', err)
-  //     })
-  // }
-
-  const networkName = chainId === 137 ? 'Mainnet' : 'Testnet'
+    network
+      .then((result) => {
+        setId({
+          chainId: result.chainId,
+          address: getAddress[0],
+          network: result.name
+        });
+      })
+      .catch((err) => {
+        console.log("Message Error >>> ", err);
+      });
+  };
 
   const addNetwork = (params) => {
     window.ethereum
       .request({
-        method: 'wallet_addEthereumChain',
-        params,
+        method: "wallet_addEthereumChain",
+        params
       })
       .then(() => {
-        console.log(`Switch to ${params[0].chainName}`)
-        setChainId(parseInt(params[0].chainId))
+        console.log(`Switch to ${params[0].chainName}`);
+        setChainId(parseInt(params[0].chainId));
       })
       .catch((err) => {
-        console.log('Message Error >>> ', err)
-      })
-  }
+        console.log("Message Error >>> ", err);
+      });
+  };
 
   const addPolygon = async (e) => {
-    //await netWork()
+    await netWork();
     await addNetwork([
       {
-        chainId: '0x64',
-        chainName: 'xDai',
+        chainId: "0x64", // esto esta harco la info tiene que ser dinamica
+        chainName: "xDai",
         nativeCurrency: {
-          name: 'xDai',
-          symbol: 'xDai',
-          decimals: 18,
+          name: "xDai",
+          symbol: "xDai",
+          decimals: 18
         },
-        rpcUrls: ['https://rpc.xdaichain.com/'],
-      },
-    ])
-  }
+        rpcUrls: ["https://rpc.xdaichain.com/"]
+      }
+    ]);
+  };
 
   return (
     <div>
       <Box p={25}>
         {chainId && (
           <div>
-            <p>ChainId: {chainId} </p>
-            <p>Name Network: {networkName} </p>
+            <p>ChainId: {id?.chainId} </p>
+            <p>Name Network: {id?.network} </p>
+            <p>My Address: {id?.address} </p>
           </div>
         )}
         <Button colorScheme="blue" onClick={addPolygon}>
-          Agregar Network xDai a Metamask
+          Add xDai Network
         </Button>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonAdd
+export default ButtonAdd;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
 const ButtonAdd = dynamic(
   () => {
-    return import('../components/button')
+    return import("../components/button");
   },
-  { ssr: false },
-)
+  { ssr: false }
+);
 
 export default function Home() {
   return (
     <div>
-      <ButtonAdd chain="0x64" chainName="xDai" symbol="xDai" rpc="https://rpc.xdaichain.com/" />
+      <ButtonAdd
+        chain="0x64"
+        chainName="xDai"
+        symbol="xDai"
+        rpc="https://rpc.xdaichain.com/"
+      />
     </div>
-  )
+  );
 }
